ff4d4b64efcc78abcb229f2039484f01485e598a
Paige McKenzie <paige.mckenzie95@yahoo.com>
2020-11-30 22:53:42 -0500

    day14 pt. 2

--- day14.py
+++ day14.py
@@ -8,21 +8,33 @@ def parse_instructions(txt):
     
     return {**inputs, **outputs}
     
-def build_LP(instructions):
-    '''Uses linear program approach to minimize amount of ORE required, while producing
-    1 FUEL, using the exchange rates given in input instructions'''
+def build_LP(instructions, day_1=True):
+    '''For day 1, uses linear program approach to minimize amount of ORE required, while producing
+    1 FUEL, using the exchange rates given in input instructions
+    For day 2, uses linear program approach to maximize amount of FUEL produced, while using
+    1 trillion ORE'''
     
     import pulp
     pulp.LpSolverDefault.msg = 0 # turn off noise
         
     # create problem
-    problem = pulp.LpProblem('required_ORE', pulp.LpMinimize)
+    if day_1:
+        # for day 1, minimize ORE
+        problem = pulp.LpProblem('required_ORE', pulp.LpMinimize)
+    else:
+        # for day 2, maximize FUEL
+        problem = pulp.LpProblem('produced_FUEL', pulp.LpMaximize)
 
     # add variables (# of times each reaction runs and amount left over for each type)
     variables = {**{'Reaction_{}'.format(i):pulp.LpVariable('Reaction_{}'.format(i), cat='Integer') for i in range(len(instructions))}, **{col:pulp.LpVariable(col, cat='Integer') for col in instructions.columns}}
 
-    # set goal (minimize ORE used)
-    problem += variables['ORE']
+    # set goal
+    if day_1:
+        # minimize ORE used
+        problem += variables['ORE']
+    else:
+        # maximize FUEL produced
+        problem += variables['FUEL']
 
     # no reaction can be run backwards
     for variable in variables.values():
@@ -37,15 +49,22 @@ def build_LP(instructions):
         # how much is left over
         problem += (variables[col]+pulp.lpDot(instructions[col].apply(lambda x:x if x<0 else 0), variables.values())>=0)
         
-    # must make 1 fuel
-    problem += (variables['FUEL']>=1)
+    if day_1:
+        # must make 1 fuel
+        problem += (variables['FUEL']>=1)
+    else:
+        # start with 1 trillion ORE
+        problem += (variables['ORE']==1000000000000)
     
     # solve problem
     status = problem.solve()
     
     # make sure program converged
     assert pulp.LpStatus[status]=='Optimal'
-    return round(variables['ORE'].value())
+    if day_1:
+        return round(variables['ORE'].value())
+    else:
+        return round(variables['FUEL'].value())
     
 def main():
     with open('day14.txt', 'r') as f:
@@ -56,7 +75,10 @@ def main():
     instructions = pd.DataFrame([parse_instructions(datum) for datum in data], dtype=int).fillna(0).astype(int)
     
     # day 1
-    print(build_LP(instructions))
+    print(build_LP(instructions, day_1=True))
+    
+    # day 2
+    print(build_LP(instructions, day_1=False))
     
 if __name__=='__main__':
     main()
